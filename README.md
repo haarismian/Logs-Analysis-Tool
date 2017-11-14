6762bee4f0bd4b0a32831cc4aae0b17da9521af4
Haaris Mian <mail@haarismian.com>
2017-11-14 12:48:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# News Database Logs Analysis!
+
+![](https://imgur.com/a/QoNTM)
+
+This project uses Postgresql and the Python DB-API to run queries on the database for a news website!
+
+# Features!
+
+  - Always up to date list of movies fomr The Movie Database's Most Popular section!
+  - HD trailers from Youtube!
+
+### Tech
+
+This project uses the following technologies to operate:
+
+* Python DB-API - for the backend to connect and run queries from a single file.
+* Postgresql - Relational database management system to manage the database and run queries
+
+### Installation
+
+This project requires the News database to be running
+
+### Usage
+1. Extract the contents of News Logs Analysis.zip to any folder
+2. logsAnalysis.py must be in the same folder as the database
+3. Run logsAnalysis.py
+4. Enjoy the premade queries!
+
+### Known Issues and To do
+N/A
---
+++ logsAnalysis.py
@@ -0,0 +1,62 @@
+import psycopg2
+
+
+def topThreeArticles():
+    connection = psycopg2.connect("dbname=news")
+    cursor = connection.cursor()
+    cursor.execute(
+        """SELECT title, count(title) FROM log INNER JOIN articles ON log.path
+         LIKE '%'|| articles.slug group by title order by count(title) desc
+         limit 3;"""
+    )
+    response = cursor.fetchall()
+    print("The top 3 articles are:")
+    for row in response:
+        print("\"" + str(row[0]) + "\" with " + str(row[1]) + " views")
+    print("")
+    cursor.close()
+    connection.close()
+
+
+def topFiveAuthors():
+    connection = psycopg2.connect("dbname=news")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+            SELECT name, count(name) FROM log inner join articles ON log.path
+            LIKE '%'|| articles.slug inner join authors on
+            articles.author=authors.id group by name order by count(name) desc;
+            """
+    )
+    response = cursor.fetchall()
+    print("The top 5 authors are:")
+    for row in response:
+        print("\"" + str(row[0]) + "\" with " + str(row[1]) + " views")
+    print("")
+    cursor.close()
+    connection.close()
+
+
+def errorCheck():
+    connection = psycopg2.connect("dbname=news")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+            SELECT * FROM(SELECT date_trunc('day',log.time) as day,
+            ROUND(100*COUNT(case when status!='200 OK' then log.time end)/
+            COUNT(case when status='200 OK' then log.time end),2) as percentage
+            FROM log log group by day) as foo where percentage>1;
+            """
+    )
+    response = cursor.fetchall()
+    print("The following days had more than 1% load errors:")
+    for row in response:
+        print(str(row[0]) + " with " + str(row[1]) + "% errors")
+    print("")
+    cursor.close()
+    connection.close()
+
+
+topThreeArticles()
+topFiveAuthors()
+errorCheck()
